5b0ad537a98576f512a999cb3de5429abfbd874b
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-19 09:51:05 +0000

    all Jest test cases passed

--- script2.test.js
+++ script2.test.js
@@ -22,7 +22,7 @@ it('calls swapi to fetch people with a promise', () => {
 })
 
 
-
+//Using mock(spie) fuctions to spie on what is happening with the promise
 it('getPeople returns counts and results', () => {
     const mockFetch = jest.fn().mockReturnValue(Promise.resolve({
         json: () => Promise.resolve({
@@ -32,10 +32,12 @@ it('getPeople returns counts and results', () => {
     }))
 
     //always use expect.assertions to make sure the assertions are being called
-    expect.assertions(2)
+    expect.assertions(4)
     return swapi.getPeoplePromise(mockFetch).then(data => {
         expect(mockFetch.mock.calls.length).toBe(1)
         expect(mockFetch).toBeCalledWith('https://swapi.py4e.com/api/people');
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(5)
     })
 })
 
