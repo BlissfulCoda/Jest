82175864b100dd3296f860391367462f10adb63e
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-19 09:39:47 +0000

    added a mock function for the fetch

--- script2.test.js
+++ script2.test.js
@@ -21,5 +21,24 @@ it('calls swapi to fetch people with a promise', () => {
     })
 })
 
+
+
+it('getPeople returns counts and results', () => {
+    const mockFetch = jest.fn().mockReturnValue(Promise.resolve({
+        json: () => Promise.resolve({
+            count: 87,
+            results: [0,1,2,3,4,5]
+        })
+    }))
+
+    //always use expect.assertions to make sure the assertions are being called
+    expect.assertions(1)
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+        expect(mockFetch.mock.calls.length).toBe(1)
+    })
+})
+
+
+
 //Jest Cheat Sheet - Really useful for running perfect tests
 //https://github.com/sapegin/jest-cheat-sheet
