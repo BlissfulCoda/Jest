281237b77f81f889bb99e5586d9d5d8436b30ca3
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-19 07:57:07 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const googleDatase = [
+    'cats.com',
+    'soups.com',
+    'flowers.com',
+    'happymeals.com',
+    'prestzels.com'
+]
+
+const googleSearch = searchInput => {
+    const matches = googleDatase.filter(website => {
+        return website.includes(searchInput);
+    })
+    return matches.length > 2 ? matches.slice(0, 2) : matches;
+}
+
+console.log(googleSearch('flower'))
